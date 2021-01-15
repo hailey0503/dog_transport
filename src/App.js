286be207896385533776630d51eaa8700d0d98ce
HaileyPong <haileypong@berkeley.edu>
2021-01-15 07:32:49 -0800

    change html input to bootstrap form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
 //import Dog from "./dog_in_a_bag.jpg";
 
 
@@ -8,16 +11,23 @@ function App() {
     <div>
     {/* <img src={Dog} alt="website logo" /> */}
       <div className="App">
-        <label for="destination">Destination</label>
+      <Form>
+        <Form.Group controlId="location">
+          <Form.Label>Destination</Form.Label>
+          <Form.Control type="text" placeholder="Destination" />
+        </Form.Group>
 
-        <input type="text" name="destination" />
+        <Form.Group controlId="date">
+          <Form.Label>Date</Form.Label>
+          <Form.Control type="date" id="start" name="trip-start" value="2021-01-22"
+          min="2021-01-01" max="2021-12-31" />
+        </Form.Group>
 
-        <input type="date" id="start" name="trip-start"
-        value="2021-01-22"
-        min="2021-01-01" max="2021-12-31" />
-        <p>
-          <button type="button">Search</button>
-        </p>
+        <Button variant="info">Search</Button>{' '}
+
+      </Form>
+
+
       </div>
     </div>
   );
