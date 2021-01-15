c3e3ba28fd133a80025de60173e9cf5aea85040c
HaileyPong <haileypong@berkeley.edu>
2021-01-15 09:12:45 -0800

    align form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 //import Dog from "./dog_in_a_bag.jpg";
 
 
@@ -11,22 +13,35 @@ function App() {
     <div>
     {/* <img src={Dog} alt="website logo" /> */}
       <div className="App">
-      <Form>
-        <Form.Group controlId="location">
-          <Form.Label>Destination</Form.Label>
-          <Form.Control type="text" placeholder="Destination" />
-        </Form.Group>
+        
+        <Container>
+          <Form>
+            <Form.Row className="align-items-center">
+              <Col xs= {4}>
+                <Form.Label htmlFor="inlineFormInput" srOnly>
+                  Destination
+                </Form.Label>
+                
+                  <Form.Control className="mb-2" type="text" placeholder="Destination" />
+               
+              </Col>
 
-        <Form.Group controlId="date">
-          <Form.Label>Date</Form.Label>
-          <Form.Control type="date" id="start" name="trip-start" value="2021-01-22"
-          min="2021-01-01" max="2021-12-31" />
-        </Form.Group>
-
-        <Button variant="info">Search</Button>{' '}
-
-      </Form>
-
+              <Col xs= {4}>
+                <Form.Label htmlFor="inlineFormInputGroup" srOnly>
+                  Data
+                </Form.Label>
+                
+                 
+                  <Form.Control className="mb-2" type="date" id="start" name="trip-start" value="2021-01-22"
+                  min="2021-01-01" max="2021-12-31" />
+                
+              </Col>
+              <Col xs= "auto">
+              <Button type="info" className="mb-2"> Submit</Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>
 
       </div>
     </div>
